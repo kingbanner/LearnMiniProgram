a86eea057fe0fd4f1da4ebf8def63c7acdf0b31e
kingbanner <kingbanner2008@gmail.com>
2020-03-27 19:48:57 +0800

    0327_eve

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
 // pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  data:{name:'MuYu Young',
+        age:18,
+       students:[
+          {id:110,name:'jordan',age:30},
+          {id:111,name:'kobe',age:25},
+          {id:112,name:'james',age:20},
+          {id:113,name:'antony',age:22}
+       ],
+       counter:0
+    },
+    handleBtnClick() {
+      //this.data.counter +=1
+      //console.log(this.data.counter)
+       this.setData({counter: this.data.counter+ 1})
+        },
+    handleSubtraction() {
+        this.setData({counter: this.data.counter -1})
+        }
+  
 })
